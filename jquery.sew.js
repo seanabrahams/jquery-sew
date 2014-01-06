2586298906386e85764e46888d1fe5f8dc4b1f35
Sean Abrahams <abrahams@gmail.com>
2014-01-06 08:52:06 -0800

    Indentation.

--- jquery.sew.js
+++ jquery.sew.js
@@ -203,9 +203,9 @@
         return false;
       }
 
-      return  val === "" ||
-              e.val.toLowerCase().indexOf(val.toLowerCase()) >= 0 ||
-              (e.meta || "").toLowerCase().indexOf(val.toLowerCase()) >= 0;
+      return val === "" ||
+             e.val.toLowerCase().indexOf(val.toLowerCase()) >= 0 ||
+             (e.meta || "").toLowerCase().indexOf(val.toLowerCase()) >= 0;
     }, this));
 
     if(vals.length) {
